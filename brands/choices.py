3696030aa2eea8fd07170b10811e9cb66c841e39
Mofe Odujirin <thebaggieboy@protonmail.com>
2023-11-17 18:48:18 +0100

    Updated collection type

--- brands/choices.py
+++ brands/choices.py
@@ -14,6 +14,11 @@ COMMUNITY_TYPE = (
     ('Accessories', 'Accessories'),
     ('Arts', 'Arts'),
     ('Footwears & Canvas', 'Footwears & Canvas'),
+    ('Enigmas', 'Enigmas'),
+    ('Watches', 'Wristwatches & Handbands'),
+    ('Skates', 'Skates'),
+    ('Cap', 'Cap'),
+    ('Masks', 'Masks'),
 )
 CLOTHING_CATEGORY= (
     ('Tops', 'Tops'),
@@ -29,6 +34,15 @@ CLOTHING_CATEGORY= (
     ('Sunglasses', 'Sunglasses'),
     ('Hats and Caps', 'Hats and Caps'),
     ('Belts', 'Belts'),
+    ('Kicks & Slides', 'Kicks & Slides'),
+    ('Masks', 'Masks'),
+    ('Lumberjacks & Vintage', 'Lumberjacks and Vintage'),
+    ('Piercings & Studs', 'Piercings & Studs'),
+    ('Baggy Wears', 'Baggy Wears'),
+    ('Tattoos', 'Tattoos'),
+    ('Chains & Necklaces', 'Chains & Necklaces'),
+    
+
 )
 
 ACCESSORIES_CATEGORY = (
