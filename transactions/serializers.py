180095212cc98242a1fe02edd6194f33f776ab43
Mofe Odujirin <thebaggieboy@protonmail.com>
2024-03-08 13:12:25 +0100

    dhge

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -4,20 +4,20 @@ from .models import *
 class OrderSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Order
-        fields = ['order_date',  'name_of_item',  'user_email',  'name_of_brand', 'amount_per_item', 'quantity', 'tracking_number', 'number_of_items', 'address', 'ordered', 'delivered']
+        fields = ['id', 'order_date',  'name_of_item',  'user_email',  'name_of_brand', 'amount_per_item', 'quantity', 'tracking_number', 'number_of_items', 'address', 'ordered', 'delivered']
 
 
 
 class PaymentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Payment
-        fields = [ 'user_email', 'full_name', 'paystack_charge_id',  'amount', 'status', 'timestamp']
+        fields = [ 'id', 'user_email', 'full_name', 'paystack_charge_id',  'amount', 'status', 'timestamp']
 
 
 class CouponSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Coupon
-        fields = [ 'code', 'amount']
+        fields = [ 'id', 'code', 'amount']
 
 
 
