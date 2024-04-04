b8a2cac53bc078ad42d1fea45f0f8af6c9e98973
Mofe Odujirin <thebaggieboy@protonmail.com>
2024-04-04 10:09:43 +0100

    added origin

--- altclan/settings.py
+++ altclan/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-#ioqb6nvy+hwu5(%r@(dv!6zztc7vb*+j63uie9ruvhihr5*oe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['altclanui.vercel.app', 'altclan-brands-api.onrender.com', 'localhost:3000', '127.0.0.1:3000', 'localhost', '127.0.0.1', 'altclan.store']
+ALLOWED_HOSTS = ['altclanui.vercel.app', 'altclanstore.vercel.app', 'altclan-brands-api.onrender.com', 'localhost:3000', '127.0.0.1:3000', 'localhost', '127.0.0.1', 'altclan.store']
 
 
 # Application definition
@@ -175,7 +175,7 @@ SESSION_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 CSRF_COOKIE_SECURE = True
-CSRF_TRUSTED_ORIGINS = ['https://altclanui.vercel.app/', 'https://altclan-api-v1.onrender.com', 'http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:8000/', 'http://127.0.0.1:8000/', 'https://altclan.store', 'altclan.store']
+CSRF_TRUSTED_ORIGINS = ['https://altclanui.vercel.app/', 'https://altclanstore.vercel.app/', 'altclanstore.vercel.app/', 'https://altclan-api-v1.onrender.com', 'http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:8000/', 'http://127.0.0.1:8000/', 'https://altclan.store', 'altclan.store']
 
 REST_AUTH = {
     
@@ -208,6 +208,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://altclan-api-v1.onrender.com",
     "https://altclanui.vercel.app",
     'https://altclan.store',
+    'https://altclanstore.vercel.app/'
 
 ]
 
